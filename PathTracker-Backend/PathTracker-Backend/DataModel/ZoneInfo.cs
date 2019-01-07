6effa89f98d38f4728b7ebc721cc6578960dd96b
Emil Kongsgaard <emilkonge888@gmail.com>
2019-01-07 17:19:06 +0100

    changes

---
+++ PathTracker-Backend/PathTracker-Backend/DataModel/ZoneInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PathTracker_Backend {
+
+    [Serializable]
+    public class ZoneInfo {
+        
+        [BsonElement(elementName: "monsterLevel")]
+        [JsonProperty("monsterLevel")]
+        public string monsterLevel { get; set; }
+
+        [BsonElement(elementName: "delveDepth")]
+        [JsonProperty("delveDepth")]
+        public string delveDepth { get; set; }
+
+    }
+    
+}
+
